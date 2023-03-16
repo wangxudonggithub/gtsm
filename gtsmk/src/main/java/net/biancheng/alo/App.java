77aaab4632b1147e64b05626ddc5e1e36e971730
wxd <wangxddg@163.com>
2023-03-16 19:42:23 +0800

    -m "dddddddd"

---
+++ gtsmk/src/main/java/net/biancheng/alo/App.java
@@ -0,0 +1,15 @@
+package net.biancheng.alo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+
+        System.out.println( "Hello World!" );
+    }
+}
